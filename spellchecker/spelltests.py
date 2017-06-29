e7950ce5f027257de2078d51dbf656827c9c5ab9
Simon Twist <10369980@mydbs.ie>
2017-06-29 12:06:53 +0100

    handle multiple failed fords

--- spellchecker/spelltests.py
+++ spellchecker/spelltests.py
@@ -10,12 +10,19 @@ class TestSpellChecker(unittest.TestCase):
 		self.spellChecker.load_words('spell.words')
 
 	def test_spell_checker(self):
-		self.assertTrue(self.spellChecker.check_word("zygotic"))
-		self.assertFalse(self.spellChecker.check_words("zygotic mistasdas elementary"))
-		self.assertTrue(self.spellChecker.check_words("our first correct sentence"))
-		self.assertTrue(self.spellChecker.check_words("Our first correct sentence"))
-		self.assertTrue(self.spellChecker.check_words("Our first correct sentence."))
-		self.assertTrue(self.spellChecker.check_words("zygotic mistasdas spelllling elementary"))
+		self.assertEqual(True,self.spellChecker.check_word("zygotic"))
+		
+		failed_words=self.spellChecker.check_words("zygotic mistasdas elementary")
+		self.assertEqual(1,len(failed_words))
+		self.assertEqual("mistasdas",failed_words[0])
+		self.assertEqual(0,len(self.spellChecker.check_words("our first correct sentence")))
+		self.assertEqual(0,len(self.spellChecker.check_words("Our first correct sentence")))
+		self.assertEqual(0,len(self.spellChecker.check_words("Our first correct sentence.")))
+		
+		failed_words=self.spellChecker.check_words("zygotic mistasdas spelllleeeing elementary")
+		self.assertEqual(2,len(failed_words))
+		self.assertEqual("mistasdas",failed_words[0])
+		self.assertEqual("spelllleeeing",failed_words[1])
 
 if __name__ == "__main__":
 	unittest.main()
